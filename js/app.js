6d67b3302c571b36e0ef39c46e7e90cb37bc29ef
Eugene <65033002+EGalinevsky@users.noreply.github.com>
2021-06-23 23:16:16 +0300

    experience

--- js/app.js
+++ js/app.js
@@ -3,7 +3,8 @@ const navToggle = document.querySelector('#nav_toggle')
 const navClose = document.querySelector('#nav_close')
 const navLink = document.querySelectorAll('.nav__link')
 
-console.log(navLink)
+
+console.log(window.scrollY)
 
 if (navToggle) {
     navToggle.addEventListener('click', () => {
@@ -21,3 +22,22 @@ function linkAction() {
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(L => L.addEventListener('click', linkAction))
+
+function scrollHeader(){
+    const nav = document.querySelector('#header')
+    if(this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+
+window.addEventListener('scroll', scrollHeader)
+
+// SCRIPT DISCOVERE
+var swiper = new Swiper(".discover__container", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop:true,
+    coverflowEffect: {
+      rotate: 0,
+    }
+  });
