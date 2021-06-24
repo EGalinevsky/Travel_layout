b33ba5fd02cf3f783ff0d9c911e291a1f2b3da83
Eugene <65033002+EGalinevsky@users.noreply.github.com>
2021-06-24 22:58:30 +0300

    70%

--- js/app.js
+++ js/app.js
@@ -23,9 +23,9 @@ function linkAction() {
 }
 navLink.forEach(L => L.addEventListener('click', linkAction))
 
-function scrollHeader(){
+function scrollHeader() {
     const nav = document.querySelector('#header')
-    if(this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+    if (this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
 }
 
 window.addEventListener('scroll', scrollHeader)
@@ -36,8 +36,33 @@ var swiper = new Swiper(".discover__container", {
     grabCursor: true,
     centeredSlides: true,
     slidesPerView: "auto",
-    loop:true,
+    loop: true,
     coverflowEffect: {
-      rotate: 0,
+        rotate: 0,
     }
-  });
+});
+
+const videoFile = document.querySelector('#video-file')
+const videoButton = document.querySelector('#video-button')
+const videoIcon = document.querySelector('#video-icon')
+
+function playPause() {
+    if (videoFile.paused) {
+        videoFile.play()
+        videoIcon.classList.add('ri-pause-line')
+        videoIcon.classList.remove('ri-play-line')
+    } else {
+        videoFile.pause()
+        videoIcon.classList.remove('ri-pause-line')
+        videoIcon.classList.add('ri-play-line')
+    }
+}
+
+videoButton.addEventListener('click', playPause)
+
+function finalVideo() {
+    videoIcon.classList.remove('ri-pause-line')
+    videoIcon.classList.add('ri-ply-line')
+}
+
+videoFile.addEventListener('ended', finalVideo)
